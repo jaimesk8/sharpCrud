e40c91cc863b055d9c19f1b7b71afdfb88897d87
jaime marinho <102005280+jaimesk8@users.noreply.github.com>
2023-04-09 15:55:25 +0100

    Add files via upload

---
+++ deleteFolder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace deleteFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"/home/jaime/Desktop/teste";
+            Directory.Delete(folderPath);
+        }
+    }
+}
---
+++ verificaFolder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace verificaFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"/home/jaime/Desktop/redes2";
+            bool folderExists = Directory.Exists(folderPath);
+            Console.WriteLine(folderExists);
+        }
+    }
+}
