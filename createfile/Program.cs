acb14f4b3d501cf768bc987a20c9dcda5fca1d96
jaime marinho <102005280+jaimesk8@users.noreply.github.com>
2023-04-09 15:46:01 +0100

    Add files via upload

---
+++ createfile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace createfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"/home/jaime/Desktop/ex.txt";
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                sw.WriteLine("This is an example text file.");
+            }   
+        }
+    }
+}
---
+++ write/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace write
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string filePath = @"/home/jaime/Desktop/ex.txt";
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine("This is another line added to the file.");
+            }
+        }
+    }
+}
